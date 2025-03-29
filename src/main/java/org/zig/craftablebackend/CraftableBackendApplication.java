43cd66fa597a7e3ebbec01c6b574ceb4266136c8
Tooclose21 <alasuper.as@gmail.com>
2025-03-29 09:28:17 +0100

    Initial commit

---
+++ src/main/java/org/zig/craftablebackend/CraftableBackendApplication.java
@@ -0,0 +1,13 @@
+package org.zig.craftablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftableBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftableBackendApplication.class, args);
+    }
+
+}
