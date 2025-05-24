b5baca2ea86f151249fb048d40679c4ca6e63671
Tooclose21 <alasuper.as@gmail.com>
2025-05-24 11:59:01 +0200

    Finished portfolio item

--- src/main/java/org/zig/craftablebackend/controller/CreatorRegisterController.java
+++ src/main/java/org/zig/craftablebackend/controller/CreatorRegisterController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zig.craftablebackend.infrastructure.dto.CreatorDto;
-import org.zig.craftablebackend.dto.CreatorDto;
-import org.zig.craftablebackend.dto.CreatorRegisterDto;
 import org.zig.craftablebackend.service.CreatorRegisterService;
 
 @RestController
